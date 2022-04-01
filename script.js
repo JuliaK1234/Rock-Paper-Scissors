1567f8c247b114bb7c0ebfb85f8d62a4221a07f5
JuliaK1234 <92735877+JuliaK1234@users.noreply.github.com>
2022-04-01 10:30:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Complete game function //
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+// Function to grab user input and run function to evaluate choices //
+  const playGame = () => {
+    //querySelector() looks through the entire html and selects the thing in parentheses//
+    const rockBtn = document.querySelector('.rock');
+    const paperBtn = document.querySelector('.paper');
+    const scissorsBtn = document.querySelector('.scissors');
+  }
+}
