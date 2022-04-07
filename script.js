4a4b15b0c67105c8cf9f8fb2defaad8d7b95c308
JuliaK1234 <92735877+JuliaK1234@users.noreply.github.com>
2022-04-07 11:55:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 // Complete game function //
-const game = () => {
+
   let playerScore = 0;
   let computerScore = 0;
 
-// Function to grab user input and run function to evaluate choices //
-  const playGame = () => {
-    //querySelector() looks through the entire html and selects the thing in parentheses//
-    const rockBtn = document.querySelector('.rock');
-    const paperBtn = document.querySelector('.paper');
-    const scissorsBtn = document.querySelector('.scissors');
-  }
+  let compChoice;
+  let playerChoice;
+
+//  grab user input and run function to evaluate choices //
+//querySelector() looks through the entire html and selects the thing in parentheses//
+  let pScoreboard = document.querySelector('.p-count');
+  let cScoreboard = document.querySelector('.c-count');
+  let roundResult = document.querySelector('.result');
+
+function computerChoice() {
+  const computerOptions = ["rock", "paper", "scissor"];
+  const randomNum = Math.floor(Math.random() * 3);
+  compChoice = computerOptions[randomNum];
+}
+
+function throwRock() {
+  playerChoice = '.rock';
+  computerChoice();
+  game();
+}
+
+let game;
+
+switch (game) {
+  case "playerChoice = 'rock' && computerChoice = 'paper'":
+    console.log(computerScore++);
+    break;
+  case "playerChoice = 'rock' && computerChoice = 'scissor'":
+    console.log(playerScore++);
+    break;
+  case "playerChoice = 'rock' && computerChoice = 'rock'":
+    console.log('Tie!');
+    break;
 }
