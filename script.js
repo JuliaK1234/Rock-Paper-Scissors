3cf2703d7c97e82f3d38bceca8599a99e0900f59
JuliaK1234 <92735877+JuliaK1234@users.noreply.github.com>
2022-04-12 10:14:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Complete game function //
 
-  let playerScore = 0;
-  let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-  let compChoice;
-  let playerChoice;
+let compChoice;
+let playerChoice;
 
 //  grab user input and run function to evaluate choices //
 //querySelector() looks through the entire html and selects the thing in parentheses//
-  let pScoreboard = document.querySelector('.p-count');
-  let cScoreboard = document.querySelector('.c-count');
-  let roundResult = document.querySelector('.result');
+let pScoreboard = document.querySelector('.p-count');
+let cScoreboard = document.querySelector('.c-count');
+let roundResult = document.querySelector('.result');
 
 function computerChoice() {
   const computerOptions = ["rock", "paper", "scissor"];
@@ -18,22 +18,64 @@ function computerChoice() {
   compChoice = computerOptions[randomNum];
 }
 
+function playerWins() {
+  playerScore++;
+  pScoreboard.innerHTML = playerScore;
+  roundResult.innerHTML = 'Player wins';
+}
+
+function compWins() {
+  computerScore++;
+  console.log(computerScore);
+  cScoreboard.innerHTML = computerScore;
+  roundResult.innerHTML = 'Computer wins';
+}
+
+function tie() {
+  roundResult.innerHTML = 'Tie!';
+}
+
 function throwRock() {
-  playerChoice = '.rock';
+  playerChoice = 'rock';
   computerChoice();
   game();
 }
 
-let game;
-
-switch (game) {
-  case "playerChoice = 'rock' && computerChoice = 'paper'":
-    console.log(computerScore++);
-    break;
-  case "playerChoice = 'rock' && computerChoice = 'scissor'":
-    console.log(playerScore++);
-    break;
-  case "playerChoice = 'rock' && computerChoice = 'rock'":
-    console.log('Tie!');
-    break;
+function throwPaper() {
+  playerChoice = 'paper';
+  computerChoice();
+  game();
 }
+
+function throwScissors() {
+  playerChoice = 'scissor';
+  computerChoice();
+  game();
+}
+
+
+function game() {
+  if (playerChoice === 'rock' && compChoice === 'paper') {
+      compWins();
+    } else if (playerChoice === 'rock' && compChoice === 'scissor') {
+      playerWins();
+    } else if (playerChoice === 'paper' && compChoice === 'rock') {
+      playerWins();
+    } else if (playerChoice === 'paper' && compChoice === 'scissor') {
+      compWins();
+    } else if (playerChoice === 'scissor' && compChoice === 'paper') {
+      playerWins();
+    } else if (playerChoice === 'scissor' && compChoice === 'rock') {
+      compWins();
+    } else if (playerChoice === compChoice) {
+      tie();
+    }
+  }
+
+  function resetScore() {
+    playerScore = 0;
+    computerScore = 0;
+    pScoreboard.innerHTML = playerScore;
+    cScoreboard.innerHTML = computerScore;
+    roundResult.innerHTML = 'Who will win?';
+  }
